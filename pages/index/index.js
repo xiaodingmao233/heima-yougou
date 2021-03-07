4e49c0ec50bae4c0bea56f9bb72401ab89e2d5ba
lizheng <2112276441@qq.com>
2021-03-07 16:48:13 +0800

    后端接口调用&轮播图功能的实现

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,22 @@
 //Page Object
 Page({
   data: {
-    
+    // 轮播图数组
+    swiperList: []
   },
-  //options(Object)
+  // 页面开始加载 就会触发
   onLoad: function(options) {
-    
+    // 1 发送异步请求获取轮播图数据
+    var reqTask = wx.request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+      success: (result) => {
+        // console.log(result.data.message)
+        this.setData({
+          swiperList: result.data.message
+        })
+      }
+    });
+      
   },
   onReady: function() {
     
